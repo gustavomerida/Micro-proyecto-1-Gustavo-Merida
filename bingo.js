724ecb811c4faa87732ab92fe5ca8fef3fc2ec89
Gustavo <gmerida@correo.unimet.edu.ve>
2024-02-11 13:31:47 -0430

    Añadir funcionalidades faltantes del juego

--- bingo.js
+++ bingo.js
@@ -32,6 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('Start').addEventListener("click", function() {
             // Código a ejecutar cuando se haga clic en el botón
             let boardSize = document.getElementById('Size').value;
+            let storedPlayers = JSON.parse(localStorage.getItem("jugadores"));
             if (boardSize) {
                 distribuirCartones(storedPlayers, boardSize);
                 drawCard(boardSize, storedPlayers.jugador1, "board");
@@ -55,6 +56,10 @@ document.addEventListener("DOMContentLoaded", function() {
                 matrices: {},
                 fichas_sacadas: []
             };
+            let storedPlayers = JSON.parse(localStorage.getItem("jugadores"));
+            for (let jugador in storedPlayers){
+                storedPlayers[jugador].puntos = 0;
+            }
             document.getElementById("header").innerHTML = `<h1 id="title-game">Super Bingo</h1>
             <h2 id="turn">Turno: ${gamedata.turno}</h2>`;
             document.getElementById("Stop").style.display = "none";
@@ -125,6 +130,7 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById('Start').addEventListener("click", function() {
             // Código a ejecutar cuando se haga clic en el botón
             let boardSize = document.getElementById('Size').value;
+            Players = JSON.parse(localStorage.getItem("jugadores"));
             if (boardSize) {
                 distribuirCartones(Players, boardSize);
                 drawCard(boardSize, Players.jugador1, "board");
@@ -136,6 +142,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 document.getElementById("barra").style.display = "none"; 
                 document.getElementById("Fichadiv").style.display = "block";
                 localStorage.setItem("jugadores", JSON.stringify(Players));
+                console.log(JSON.stringify(Players));
             }
         });
                
@@ -147,6 +154,10 @@ document.addEventListener("DOMContentLoaded", function() {
                 matrices: {},
                 fichas_sacadas: []
             };
+            let Players = JSON.parse(localStorage.getItem("jugadores"));
+            for (let jugador in Players){
+                Players[jugador].puntos = 0;
+            }
             document.getElementById("header").innerHTML = `<h1 id="title-game">Super Bingo</h1>
             <h2 id="turn">Turno: ${gamedata.turno}</h2>`;
             document.getElementById("Stop").style.display = "none";
@@ -162,12 +173,14 @@ document.addEventListener("DOMContentLoaded", function() {
             localStorage.setItem("jugadores", JSON.stringify(Players));
         });
         distribuirCartones(Players, parseInt(boardSize));
+        localStorage.setItem("jugadores", JSON.stringify(Players));
         drawCard(parseInt(boardSize), player1, "board");
         drawCard(parseInt(boardSize), Players.jugador2, "board-2");
         drawCard(parseInt(boardSize), Players.jugador3, "board-3");
         drawCard(parseInt(boardSize), Players.jugador4, "board-4");
+        Players = JSON.parse(localStorage.getItem("jugadores"));
         drawTable(Players);
-        localStorage.setItem("jugadores", JSON.stringify(storedPlayers));
+        
         // Mostrar los elementos necesarios para empezar el juego
         document.getElementById("container").style.display = "none";
         document.getElementById("header").style.display = "block";
@@ -224,12 +237,17 @@ function drawCard (size, player, card){
 }
 
 function drawTable(players){
-    if (!players.jugador1.victorias){
-            players.jugador1.victorias = 0;
-    }  
-    if (!players.jugador1.puntos){
-        players.jugador1.puntos = 0;
-    }  
+    console.log("AQUII")
+    for(let i = 0; i<players.length; i++){
+        console.log(JSON.stringify(players[i].carton));
+        if (!players[i].victorias){
+                players[i].victorias = 0;
+        }  
+        if (!players[i].puntos || !players[i].carton){
+            players[i].puntos = 0;
+        } 
+    }
+     
         
     document.getElementById("players-table").innerHTML = `<tr>
     <td data-title="Nombre">${players.jugador1.nombre}</td>
@@ -253,27 +271,6 @@ function drawTable(players){
   </tr>`
 }
 
-// function verifyButtons(boardSize, storedPlayers){
-//     document.getElementById('Start').addEventListener("click", function() {
-//         // Código a ejecutar cuando se haga clic en el botón
-//         if (boardSize) {
-//             drawCards(boardSize, storedPlayers.jugador1.nombre);
-//             drawTable(storedPlayers);
-//             document.getElementById("Stop").style.display = "block";
-//             document.getElementById("barra").style.display = "none";   
-//         }
-//     });
-    
-//     document.getElementById('Stop').addEventListener("click", function() {
-//         // Código a ejecutar cuando se haga clic en el botón Reanudar
-        
-//         document.getElementById("Stop").style.display = "none";
-//         document.getElementById("barra").style.display = "block";
-//         drawCards(0, storedPlayers.jugador1.nombre);
-//         drawTable(storedPlayers);
-//     });
-//   };
-
 document.getElementById("Delete").addEventListener("click", function() {
 
     const confirmacion = confirm("¿Estás seguro de que deseas borrar los datos? Se perderán la información de los jugadores, incluyendo el historial de victorias.");
@@ -290,9 +287,11 @@ document.getElementById("Delete").addEventListener("click", function() {
   });
 
 document.getElementById("GameButton").addEventListener("click", function() {
-    // console.log(gamedata);
-    //Aumentar un turno (Son máximo 25)
     gamedata.turno += 1;
+    if (gamedata.turno<26){
+        // console.log(gamedata);
+    //Aumentar un turno (Son máximo 25)
+    
     // console.log(gamedata.turno);
     document.getElementById("header").innerHTML = `<h1 id="title-game">Super Bingo</h1>
     <h2 id="turn">Turno: ${gamedata.turno}</h2>`;
@@ -341,7 +340,7 @@ document.getElementById("GameButton").addEventListener("click", function() {
     }
     
     //El juego durará cómo máximo 25 turnos, pero si alguien consigue cartón lleno se termina.
-    if(gamedata.turno==25){
+    if(gamedata.turno>=25){
         let ps = {};
         for (let player in players){
             ps[player]= player.puntos;
@@ -349,46 +348,29 @@ document.getElementById("GameButton").addEventListener("click", function() {
         
         finalizarPartida(ps, "Se alcanzó el límite de turnos!", players, bingo);
     }
+    }else{
+        let bingo = []
+        calcularPuntos(gamedata, players, bingo);
+        let players;
+        let storedPlayers = JSON.parse(localStorage.getItem("jugadores"));
+        if(storedPlayers){
+            players = storedPlayers;
+        }else{
+            players= Players;
+        }
+        let ps = {};
+        for (let player in players){
+            ps[player]= player.puntos;
+        }
+        
+        finalizarPartida(ps, "Se alcanzó el límite de turnos!", players, bingo);
+    }
+    
 
 });
 
 
 function calcularPuntos(gamedata, storedPlayers, bingo){
-    // let cont = 0;
-    // let keys = Object.keys(gamedata[matrices]);
-    // for (let matriz in gamedata[matrices]) {
-        
-    //     //diagonalmente
-    //     let selected = true;
-    //     let results = [];
-    //     for (let i = 0; i < matriz.length; i++) {
-    //         if(gamedata.matriz[i][i]){
-    //             results.push(1);
-    //         }else{
-    //             results.push(0);
-    //         };
-    //       }
-    //     console.log(results);
-    //     if (results.includes(0)){
-    //         // pass;
-    //     }else{
-    //         gamedata[puntos][gamedata[matrices][keys[cont]]];
-    //     }
-    //     cont++;
-    //   }
-
-    // let keys = Object.keys(matrices);
-    // for(let matriz in keys){
-    //     let results = [];
-    //     for (let i = 0; i < matriz.length; i++) {
-    //         if(gamedata.fichas_sacadas.includes(matriz[i][i])){
-    //             results.push(1);
-    //         }else{
-    //             results.push(0);
-    //         };
-    //       }
-    //     console.log(results);
-    // }
 
     let puntosLinea = 1;
     let puntosCarton = 5;
@@ -442,6 +424,7 @@ function calcularPuntos(gamedata, storedPlayers, bingo){
         }
 
         // Verificar si se ha completado una diagonal hacia arriba
+        lineaCompleta = true;
         for (let o = carton.length -1; o >=0; o--) {
             if (!gamedata.fichas_sacadas.includes(carton[o][carton.length -1 - o])) {
                 lineaCompleta = false;
@@ -470,6 +453,7 @@ function calcularPuntos(gamedata, storedPlayers, bingo){
         if (cartonCompleto) {
             puntos = puntosCarton;
             bingo.push(storedPlayers[jugador].nombre);
+            storedPlayers[jugador].victorias +=1;
         }
         storedPlayers[jugador].puntos = puntos;
         
@@ -483,51 +467,38 @@ function finalizarPartida(storedPlayers, mensaje, sp, bingo){
     let playerspoints = new Object();
     let winner = "nadie";
 
-    //Selección de ganador y resultados:
+    //Selección de ganador y mostrar resultados:
     if (bingo.length!=0){
         if (bingo.length==1){
             winner = bingo[0];
+            alert(`Bingo!! ${winner}llenó su cartón.`);
         }else{
-            winner= bingo;
+            alert(`Bingo!!`);
+            for(let i=0;i<bingo.length;i++){
+                alert(`${bingo[i]}llenó su cartón.`);
+            }
         }
     }else{
-        console.log(`antes: ${JSON.stringify(sp)}`);
         let sortedPlayers = Object.entries(sp).sort((a, b) => b[1].puntos - a[1].puntos);
         let highestScore = sortedPlayers[0][1].puntos;
-        let winners = sortedPlayers.filter(player => player[1].puntos === highestScore).map(player => player[1].nombre);
+        let winners = sortedPlayers.filter(player => player[1].puntos === highestScore).map(player => player[1]);
         if (winners.length === 1) {
-            console.log(`El ganador es ${winners[0]} con ${highestScore} puntos.`);
+            drawTable(sp);
+            alert(`El ganador es ${winners[0].nombre} con ${highestScore} puntos.`);
+            winners[0].victorias +=1;
         } else {
-            console.log(`Hay un empate entre ${winners.join(" y ")} con ${highestScore} puntos.`);
+            console.log(winners);
+            drawTable(sp);
+            alert(`Hay un empate entre ${winners.map(winner => winner.nombre).join(" y ")} con ${highestScore} puntos.`);
+            for(let i=0;i<winners.length;i++){
+                winners[i].victorias+=1;
+            }
         }
-        console.log(`después: ${sortedPlayers}`);
     }
 
-    
-    
-
-    // for (const key in players) {
-
-    //     //Obtenemos la nueva key deseada. Que es el valor. 
-    //     let nuevaKey = players[key]
-    //     //Si el acumulador no contiene la nueva clave, la creamos y 
-    //     // la inicializamos con un arreglo vacio. 
-    //     if (!playerspoints.hasOwnProperty(nuevaKey)){
-    //         playerspoints[nuevaKey] = [];
-    //     }       
-    //     //Agregamos la key anterior al arreglo. 
-    //     playerspoints[nuevaKey].push(key)
-
-
-    // }
-
-    // playerspoints.sort((a, b) => b.puntos - a.puntos);
 
     //Se borran los puntos.
-    console.log(mensaje + `ganó ${winner}`);
-    for (let jugador in sp){
-        sp[jugador].puntos = 0;
-    }
+    console.log(mensaje + ` ganó ${winner}`);
     localStorage.setItem("jugadores", JSON.stringify(sp));
 
     // if (!players.jugador1.victorias){
@@ -536,106 +507,9 @@ function finalizarPartida(storedPlayers, mensaje, sp, bingo){
     // if (!players.jugador1.puntos){
     //     players.jugador1.puntos = 0;
     // }
-    drawTable(sp);
-}
-
     
-    
-/*let player1 = {};
-let player2= {};
-let player3= {};
-let player4= {};
-let boardSize;
-
-
-
-try {
-    //Leer localStorage
-    read_LocalStorage(player1, player2, player3, player4);
-    document.getElementById('container').innerHTML = ``;
-    document.getElementById('header').style.display = 'block';
-    document.getElementById('main').style.display = 'flex';
-    document.getElementById('board').style.display = 'grid';
-
-} catch {
-    document.getElementById('main').style.display = 'none';
-    document.getElementById('header').style.display = 'none';
-    document.getElementById('board').style.display = 'none';
-    document.getElementById('container').style.display='block';
-
-    document.getElementById('setupForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-        
-        player1.nombre = document.getElementById('player1').value;
-        player2.nombre = document.getElementById('player2').value;
-        player3.nombre = document.getElementById('player3').value;
-        player4.nombre = document.getElementById('player4').value;
-        boardSize = document.getElementById('boardSize').value;
-        
-        if (player1.nombre && player2.nombre && player3.nombre && player4.nombre && boardSize) {
-            // Aquí se pueden almacenar los nombres de los jugadores y el tamaño del cartón y proceder con la lógica del juego de Bingo
-    
-            drawCards(parseInt(boardSize), player1);
-            document.getElementById('container').style.display='none';
-            document.getElementById('header').style.display = 'block';
-            document.getElementById('main').style.display = 'flex';
-            document.getElementById('board').style.display = 'grid';
-    
-            console.log('Players:', player1, player2, player3, player4);
-            console.log('Board Size:', boardSize);
-        } else {
-            alert('Por favor, complete todos los campos.');
-        }
-    });
-    save_LocalStorage(player1, player2, player3, player4);
 }
 
-
-function createMatrix (n){
-
-    let matriz = [];
-    let all = [];
-
-    for (let i = 1; i <= n; i++) {
-        let row = [];
-        for (let i = 1; i <= n; i++) {
-            let randomNumber = Math.floor(Math.random() * 50) + 1;
-            if (all.includes(randomNumber)){
-                while (all.includes(randomNumber)){
-                    randomNumber = Math.floor(Math.random() * 50) + 1;
-                }
-            }
-            all.push(randomNumber);
-            row.push(randomNumber);  
-        }
-        matriz.push(row);
-    }
-
-    return matriz;
-}
-
-function drawCards (size, player){
-    let matriz = createMatrix(size);
-    document.getElementById('board').innerHTML = `<div class="name">${player}</div>`;
-    document.getElementById('board').innerHTML += `<div id="matrix"></div>`;
-    let matrizHTML = document.getElementById('matrix');
-    matrizHTML.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    matriz.forEach(row => row.forEach(element => matrizHTML.innerHTML += `<div class="token">${element}</div>`));
-}
-
-function read_LocalStorage(player1, player2, player3, player4){
-    player1 = JSON.parse(localStorage.getItem("player1"));
-    player2= JSON.parse(localStorage.getItem("player2"));
-    player3= JSON.parse(localStorage.getItem("player3"));
-    player4= JSON.parse(localStorage.getItem("player4"));
-}
-
-function save_LocalStorage(player1, player2, player3, player4){
-    localStorage.setItem("player1", JSON.stringify(player1));
-    localStorage.setItem("player2", JSON.stringify(player2));
-    localStorage.setItem("player3", JSON.stringify(player3));
-    localStorage.setItem("player4", JSON.stringify(player4));
-}*/
 
 function distribuirCartones(list, n){
     matrices= [createMatrix(n), createMatrix(n), createMatrix(n), createMatrix(n)];
